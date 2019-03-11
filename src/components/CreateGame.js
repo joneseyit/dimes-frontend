6715983ba2f2a8d187e4af19aeb83705982d627e
Andrew Jones <joneseyit@protonmail.com>
2019-03-11 13:52:48 -0400

    fixed error message - working on date-time form

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Form, Container, TextArea } from 'semantic-ui-react'
-
+import * as DateTime from 'react-datetime'
 class CreateGame extends React.Component {
+    state = {
+        title: '',
+        place: '',
+        time: '',
+        notes: ''
+    }
 
+    handleChange = (e) => {
+        this.setState({  })
+    }
+    
     render(){
         return (
         <Container text>
@@ -28,18 +38,36 @@ class CreateGame extends React.Component {
             <Form.Field>
             <label>Game notes</label>
             <input
-                placeholder="Notes"
+                placeholder="Is it more competitve, just for fun, etc."
                 name='notes'
                 control={TextArea}
                 onChange={this.onChangeHandler}
             />
             </Form.Field>
+
+            {/* <DatePicker
+                inline
+                selected={Date.now()}
+                onChange={this.handleChange}
+                showTimeSelect
+                timeFormat="HH:mm"
+                timeIntervals={30}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                timeCaption="time"
+            /> */}
+
+            <DateTime 
+                input={false}
+                name='time'
+            />
     
             <Form.Field>
             <input type='submit' />
             </Form.Field>
         </Form>
+
         </Container>
+        
         )
     }
 }
