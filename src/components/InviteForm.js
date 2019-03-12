4bbe24286a5775d830b6c881e1cf07e7bc3d48d0
Andrew Jones <joneseyit@protonmail.com>
2019-03-11 22:42:05 -0400

    tried setting things up blind - test tomorrow

--- src/components/InviteForm.js
+++ src/components/InviteForm.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { Form, Dropdown } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+
 
 class InviteForm extends React.Component{
     state = {
-        users: []
+        users: [],
+        message: ''
     }
+
+    submitHandler = (e) => {
+        let invitedId = e.target.invited.value
+        let curUserId = this.props.user.id
+        let gameId = e.target.game.value
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                user_id: curUserId,
+                invited_id: invitedId,
+                responded: false,
+                game_id: gameId
+
+            })
+
+        }
+
+        fetch('http://localhost:3000/invitations', options)
+        .then( res => res.json())
+        .then( console.log )
+        .then(this.setState({ message: 'You dished out an invite - nice!' }))
+    }
+
     fetchUsers = () => {
         fetch('http://localhost:3000/users')
         .then(res => res.json())
@@ -16,16 +45,29 @@ class InviteForm extends React.Component{
     }
     
     render() {
-        let users = this.state.users.length > 0 ? this.state.users.map( user => user.invite_format) : []
-        
+        // let users = this.state.users.length > 0 ? this.state.users.map( user => user.invite_format) : []
+        let users = this.state.users.map( user => user.invite_format)
+        let usersMinusMe = users.filter(user => user.id !== this.props.user.id)
+        let games = this.props.user.games.map( game => { key: game.id, value: game.id, text: game.title } )
         return (
 
             <div>
-                <Dropdown placeholder='Select User to Invite' options={users}/>
+                <p>{this.state.message}</p>
+                <Form onSubmit={this.submitHandler}>
+                    <Form.Field>
+                        <Dropdown placeholder='Which Player' name='invited' options={usersMinusMe} />
+                        <Dropdown placeholder='Which Game' name='game' options={games} />
+                        <input type='submit' value="Invite User" />
+                    </Form.Field>
+                </Form>
             </div>
             
         )
     }
 }
 
-export default InviteForm
+const mapStateToProps = (state) => {
+    return { user: state.user }
+}
+
+export default connect(mapStateToProps)(InviteForm)
